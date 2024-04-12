9a3024f46bbd7804024469ff7ae8fcbba6bb09cb
HigorDev8 <higordev8@gmail.com>
2024-04-12 14:52:58 -0300

    segundo

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Categoria é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo categoria deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String categoria;
+	
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String nome;
+	
+	@NotBlank(message = "O atributo Descrição é obrigatório!")
+	@Size(min = 5, max = 1000, message = "O atributo título deve conter no mínimo 05 e no máximo 1000 caracteres.")
+	private String descricao;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	public Long getId() {
+		return id;
+	}
+}
